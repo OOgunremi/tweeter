d847b7c92547f9f0166fb7377d8cd6c0b0973e1b
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-04 00:05:37 -0600

    fix counter paste-inactive  bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
 
 //tracks the number of characters left to type from the 140 limit and stores it.
-$(document).ready(function () {
-  $('#tweet-text').keypress(function(e) {
-    
-    ($(this).next().find('.counter')[0]['innerText'] = 139 - Number($(this)[0].textLength));
-    let $charactersLeft = 139 - Number($(this)[0].textLength);
+$(document).ready(function() {
+
+  $('#tweet-text').on('input',function(e) {
+    ($(this).next().find('.counter')[0]['innerText'] = 140 - Number($(this)[0].textLength));
+    let $charactersLeft = 140 - Number($(this)[0].textLength);
 
     //turns counter red once 140 characters limit is exceeded
-    if ($charactersLeft < -1) {
+    if ($charactersLeft < 0) {
       $(".counter").css('color', 'red');
-    }
+    } else {
+      $(".counter").css('color', '#666');
+    };
   });
 });
